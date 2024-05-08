e896d65a8834b703eb4e022a0a69a5d0df6f5c8a
Joseph Clyde Mar <clydemar234@gmail.com>
2024-05-09 03:18:53 +0800

    feat: Add video recording when the sensor senses something and upload the recorded video to the remote server

--- src/main.py
+++ src/main.py
@@ -5,41 +5,28 @@ except Exception as e:
     print(e)
     quit()
 
+import time
 import os
-import socketio # type: ignore
-import requests # type: ignore
+import asyncio
 import base64
+import aiohttp  # type: ignore
 import cv2 as cv # type: ignore
 import RPi.GPIO as GPIO # type: ignore
-from picamera2 import Picamera2 # type: ignore
 
-
+# custome modules
+from globals import TRIG_PIN, ECHO_PIN, PICAM2, TRAINING_DIR, VALIDATION_DIR, HAAR_CASCADE, SIO, HTTP_REST_ENDPOINTS, REMOTE_SERVER_HOST, VIDEO_WRITER
 import sensors
+import api_interface
 import collect_training_data
-import train_model
-import recognize_face
+# import train_model
+# import recognize_face
+
+# FACE_RECOGNIZER = cv.face.LBPHFaceRecognizer_create()
 
 
-# Set pins for trigger and echo
-TRIG_PIN = 16
-ECHO_PIN = 18
-
-PICAM2 = Picamera2()
-SIO = socketio.Client()
-HAAR_CASCADE = cv.CascadeClassifier('./haarcascades/haarcascade_frontalface_default.xml')
-FACE_RECOGNIZER = cv.face.LBPHFaceRecognizer.create()
-
-FACE_RECOGNIZER.read('model_trained.yml')
-
-TRAINING_DIR = os.path.join('captured_faces', 'train')
-VALIDATION_DIR = os.path.join('captured_faces', 'val')
-
-REMOTE_SERVER_HOST = 'http://192.168.1.2:8500'
-API_PATH_ADD_NEW_AUTHORIZED_USER = '/api/v1/authorized_users'
-API_PATH_ADD_NEW_DETECTION = '/api/v1/detections'
 
 # Set GPIO mode
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 # Setup GPIO pins
 GPIO.setup(TRIG_PIN, GPIO.OUT)
@@ -53,17 +40,8 @@ PICAM2.configure('preview')
 PICAM2.start()
 
 
-# Global variables
-is_socket_connected = False
-add_new_authorized_user = False
-new_authorized_user_data = None
 
 
-people = ['momo', 'meme', 'mimi', 'mumu', 'mama']
-#for p in os.listdir(TRAINING_DIR):
-#    people.append(p)
-print('Recognized People: ', people)
-print('Press D to Exit...')
 
 
 
@@ -79,55 +57,131 @@ def from_server_to_add_new_authorized_user(data):
 
 
 
-try:
-    currently_captured_image_count = 0
-    while True:
-        # Keep looping until connected to socket.io remote server
-        while not is_socket_connected:
-            try:
-                SIO.connect(REMOTE_SERVER_HOST)
-                print('Socket ID:', SIO.sid)
-                is_socket_connected = True
-            except Exception as e:
-                print(e)
-                continue
-        
-        # Capture image from Raspberry Pi Camera
-        frame = PICAM2.capture_array()
+# people = []
+# if os.path.exists(TRAINING_DIR):
+#     for p in os.listdir(TRAINING_DIR):
+#         people.append(p)
+# else:
+#     first_authorized_user = input('Enter First Authorized User: ')
+#     is_done_capturing = False
+#     count = 0
+#     while not is_done_capturing:
+#         frame = PICAM2.capture_array()
+#         is_successfully_captured, is_done_capturing = collect_training_data.capture_new_images(HAAR_CASCADE, frame, first_authorized_user, TRAINING_DIR, VALIDATION_DIR, count, 20)
+#         if is_successfully_captured:
+#             count += 1
+#     people.append(first_authorized_user)
+#     # train_model.train_face_recognizer(TRAINING_DIR, VALIDATION_DIR, people)
 
-        face_detection_state = 'OFF'
-        distance = sensors.get_distance(TRIG_PIN, ECHO_PIN)
-        if distance is not None: # Just for printing the distance
-            print("Distance: {:.2f} cm".format(distance))
-        if distance is not None and distance <= 150: # Filter distances below 150cm
-            face_detection_state = 'ON'
-            recognize_face.identify_face(HAAR_CASCADE, FACE_RECOGNIZER, frame, people)
-            if add_new_authorized_user:
-                add_new_authorized_user = False
-                if new_authorized_user_data != None:
-                    is_finished_capturing_images = collect_training_data.capture_new_images(HAAR_CASCADE, frame, new_authorized_user_data['name'], TRAINING_DIR, VALIDATION_DIR, currently_captured_image_count, 20)
-                    currently_captured_image_count += 1
-                    if is_finished_capturing_images:
-                        currently_captured_image_count = 0
-                        with open(f'{VALIDATION_DIR}/{new_authorized_user_data["name"]}/0.jpg', 'rb') as f:
-                            profile_image_data = base64.b64encode(f.read()).decode('utf-8')
-                            requests.post(f'{REMOTE_SERVER_HOST}{API_PATH_ADD_NEW_AUTHORIZED_USER}', data={ 'profileImage': profile_image_data, 'name': new_authorized_user_data['name'] })
-                        train_model.train_face_recognizer(TRAINING_DIR, VALIDATION_DIR, people)
-                        print(f'New Authorized User [{new_authorized_user_data["name"]}] is registered.')
+# print('Recognized People: ', people)
+# print('Press D to Exit...')
+
+
+# if os.path.exists('./model_trained.yml'):
+#     FACE_RECOGNIZER.read('./model_trained.yml')
+# else:
+#     train_model.train_face_recognizer(TRAINING_DIR, VALIDATION_DIR, people)
+
+
+
+
+async def main():
+    global VIDEO_WRITER
+    is_socket_connected = False
+    add_new_authorized_user = False
+    new_authorized_user_data = None
+    is_recording_video = False
+    try:
+        currently_captured_image_count = 0
+        async with aiohttp.ClientSession() as session:
+            while True:
+                # Keep looping until connected to socket.io remote server
+                while not is_socket_connected:
+                    try:
+                        SIO.connect(REMOTE_SERVER_HOST)
+                        print('Socket ID:', SIO.sid)
+                        is_socket_connected = True
+                    except Exception as e:
+                        print(e)
+                        continue
+                
+                # Capture image from Raspberry Pi Camera
+                frame = PICAM2.capture_array()
+                resized_frame = cv.resize(frame, (182, 102))
+
+                face_detection_state = 'OFF'
+                distance = sensors.get_distance(TRIG_PIN, ECHO_PIN)
+                if distance is not None: # Just for printing the distance
+                    print('Distance: {:.2f} cm'.format(distance))
                 else:
-                    print('no user data...')
-        cv.putText(frame, f'Face Detection State: {face_detection_state}', (30, 40), cv.FONT_HERSHEY_SIMPLEX, 0.9, (0,0,255), 2)
+                    print('Ultrasonic Sensor is not connected...')
+            
+                if distance != None and distance < 150: # Filter distances below 150cm
+                    if not is_recording_video:
+                        VIDEO_WRITER = cv.VideoWriter(os.path.join('vids', f"video_{time.strftime('%Y%m%d%H%M%S')}.avi"), cv.VideoWriter_fourcc(*'XVID'), 20.0, (1280, 720))
+                        is_recording_video = True
 
+                    face_detection_state = 'ON'
+                    # recognize_face.identify_face(HAAR_CASCADE, FACE_RECOGNIZER, frame, people)
+                    if add_new_authorized_user:
+                        if new_authorized_user_data != None:
+                            is_successfully_captured, is_done_capturing = collect_training_data.capture_new_images(HAAR_CASCADE, frame, new_authorized_user_data['name'], TRAINING_DIR, VALIDATION_DIR, currently_captured_image_count, 20)
+                            if is_successfully_captured:
+                                currently_captured_image_count += 1
+                            if is_done_capturing:
+                                currently_captured_image_count = 0
+                                resized_frame = cv.resize(frame, (182, 102))
+                                _, im_buff_arr = cv.imencode('.jpg', resized_frame)
+                                byte_frame = im_buff_arr.tobytes()
+                                profile_image_data = base64.b64encode(byte_frame).decode('utf-8')
+                                await asyncio.gather(api_interface.send_profile_user_image(session, HTTP_REST_ENDPOINTS['authorized_users_v1'], (profile_image_data, new_authorized_user_data)))
+                                print(f'New Authorized User [{new_authorized_user_data["name"]}] is registered.')
+                                add_new_authorized_user = False
+                        else:
+                            print('no user data...')
+                    if VIDEO_WRITER is not None:
+                        # print('Writing video...')
+                        VIDEO_WRITER.write(frame)
+                else:
+                    if VIDEO_WRITER is not None:
+                        # print('Saving video...')
+                        VIDEO_WRITER.release()
+                        is_recording_video = False
+                        # recently_recorded_videos =
+                    current_day_record_data = await api_interface.create_current_day_record_get_request_task(session, HTTP_REST_ENDPOINTS['day_records_v2'])
+                    request_tasks = api_interface.send_recorded_videos(session, HTTP_REST_ENDPOINTS['detections_v1'], os.listdir('vids'), current_day_record_data['_id'])
+                    for task in request_tasks:
+                        await task
+                cv.putText(frame, f'Face Detection State: {face_detection_state}', (30, 40), cv.FONT_HERSHEY_SIMPLEX, 0.9, (0,0,255), 2)
+
+                if with_gui == 1:
+                    cv.imshow('Frame', frame)
+                    if cv.waitKey(20) == ord('d'):
+                        break
+
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        if VIDEO_WRITER is not None:
+            VIDEO_WRITER.release()
         if with_gui == 1:
-            cv.imshow('Frame', frame)
-            if cv.waitKey(20) == ord('d'):
-                break
+            cv.destroyAllWindows()
 
-except KeyboardInterrupt:
-    GPIO.cleanup()
-    if with_gui == 1:
-        cv.destroyAllWindows()
-    
+
+
+
+
+
+
+asyncio.run(main())
+
+
+
+
+
+
+
+
+
 
 GPIO.cleanup()
 if with_gui == 1:
